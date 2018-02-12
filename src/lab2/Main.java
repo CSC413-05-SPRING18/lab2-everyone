9d7aa841828e570d1383554398dd7002f1351d11
brianxautumn <shred@brianautumn.com>
2018-02-11 19:29:42 -0800

    Initial commit

---
+++ src/lab2/Main.java
@@ -0,0 +1,17 @@
+package lab2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+  /**
+   * A useless method that returns -1
+   * @param a a random number
+   * @return -1 because reasons
+   */
+  int brianParra(int a){
+      return -1;
+    }
+}
