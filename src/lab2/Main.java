f2e1558eeaed5ecf10e2cb634ae2615e6bfc2713
bhaveshc20 <bhaveshc789@gmail.com>
2018-02-12 20:29:34 -0800

    completed in-class lab

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -14,4 +14,14 @@ public class Main {
   int brianParra(int a){
       return -1;
     }
+
+    /**
+     * A useful method that returns 1
+     * @param a a random number
+     * @return 1 because idk
+     */
+
+    int bhaveshChowdhury(int a){
+      return 1;
+    }
 }
